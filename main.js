7ab505c94d0fa2a0fcae32e237e9ece3617e6e36
SebasVargas2 <savargaspaez@gmail.com>
2023-04-19 11:54:20 -0500

    listo

--- main.js
+++ main.js
@@ -3,37 +3,55 @@ const boton = document.getElementById('boton')
 const boton2 = document.getElementById('pause')
 const boton3 = document.getElementById('cancel')
 let pantalla = document.getElementById('pantalla')
-
+var detener2 = null
+let flag = true
 
 function start(){
     function regresiva(ingreso){
-        if (ingreso==0){
-            pantalla.textContent='Finish'
+        if (ingreso<0){
+            pass
         }
         else {
-            let detener = setInterval(regresiva,1000,ingreso-1)
-            pantalla.textContent=(ingreso)
-            boton2.addEventListener('click',()=>{
-                clearInterval(detener)
-                boton2.textContent="Continue"
-
-                boton2.addEventListener('click', ()=>{
-                    setInterval(regresiva,1000,ingreso-1)
-                    boton2.textContent="Pausar"
+            if (ingreso==0){
+                pantalla.textContent='Finish'
+            }
+            else {
+                var detener = setInterval(regresiva,1000,ingreso-1)
+                pantalla.textContent=(ingreso)
+                boton2.addEventListener('click',()=>{
+                    clearInterval(detener)
+                    boton2.textContent="Continue"
+    
+                    boton2.addEventListener('click', ()=>{
+                        detener2 = setInterval(regresiva,1000,ingreso-1)
+                        boton2.textContent="Pausar"
+                    })
                 })
-            })
-            
-            boton3.addEventListener('click',()=>{
-                clearInterval(detener)
-                ingreso.textContent=""
-                pantalla.textContent="0"
-            })
+                boton3.addEventListener('click',()=>{
+                    clearInterval(detener2)
+                    clearInterval(detener)
+                    ingreso.textContent=" "
+                    pantalla.textContent="Cancelado"
+                    flag=false
+                })
+            }
         }
     }
-    regresiva(ingreso.value)
+
+    if (isNaN(ingreso.value)){
+        alert('Eso no es un numero')
+        miNumero.value = " "
+    }
+    else{
+        if (flag==true){
+            regresiva(ingreso.value)
+        }
+    }
 }   
 
-
+function again(){
+    location.reload()
+}
 
 
 
